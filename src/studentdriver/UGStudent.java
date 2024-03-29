a623548fedec4cc9b55b7564529593eb30f72eb9
PMA2004 <156373040+PMA2004@users.noreply.github.com>
2024-03-29 17:30:46 -0500

    Added the methods for the UGStudent class

--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -2,5 +2,44 @@
 package studentdriver;
 
 public class UGStudent {
+    // Private instance varibles 
+    private double scholarshipAmount;
+    private int coursesEnrolled;
+    private boolean hasScholarship;
+    private double ADDITONAL_FEE = 820.70;
+    
+    // UGStudent
+    public UGStudent(String studentName, int StudentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int coursesEnrolled){
+        this.coursesEnrolled = coursesEnrolled;
+        this.hasScholarship = hasScholarship;
+        this.scholarshipAmount = scholarshipAmount;
+        
+    }
+    // HasScholarship method
+    public boolean isHasScholarship(){
+        // I might need to add smoe a lection structures here
+        return this.hasScholarship;
+    }
+    // Getters for the UGStudent class
+    public double getScholarshipAmount(){
+        return scholarshipAmount;
+    }
+    public int getCoursesEnrolled(){
+        return coursesEnrolled;
+    }
+    public double getPayableAmount(){
+        // Tuition fee amount calculated 
+        double tuitionCost = (coursesEnrolled * ADDITONAL_FEE);
+        double totalCost = (tuitionCost + ADDITONAL_FEE);
+        // if statmente that subtacts the schoalrship amount
+        if(hasScholarship) {
+            totalCost -= scholarshipAmount;
+        }
+        return totalCost;
+    }
+    // toString method
+    public String toString(){
+        return "";
+    }
     
 }
